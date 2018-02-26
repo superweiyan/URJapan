b312bee792563ac736e438f65ee6ae58eb58e428
linweiyan <345917048@qq.com>
2018-02-26 20:38:25 +0800

    添加APP UI 界面规范需求

---
+++ URJapan/Component/URThemeConfig.h
@@ -0,0 +1,29 @@
+//
+//  URThemeConfig.h
+//  URJapan
+//
+//  Created by lin weiyan on 26/02/2018.
+//  Copyright © 2018 URWY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface URThemeConfig : NSObject
+//@end
+
+#pragma mark - button
+
+#define URButtonNoramlHeight 20
+#define URButtonSmallHeight 15
+
+#pragma mark - icon
+
+#define URIconWidth 16
+
+#pragma mark - color
+
+#define NoramlColor  [UIColor colorWithRed:102/255.0 green:102/255.0 blue:102/255.0 alpha:1]
+#define weakColor   [UIColor colorWithRed:153/255.0 green:153/255.0 blue:153/255.0 alpha:1]
+#define importColor   [UIColor colorWithRed:53/255.0 green:53/255.0 blue:53/255.0 alpha:1]
+
+
---
+++ URJapan/Component/URThemeConfig.m
@@ -0,0 +1,14 @@
+//
+//  URThemeConfig.m
+//  URJapan
+//
+//  Created by lin weiyan on 26/02/2018.
+//  Copyright © 2018 URWY. All rights reserved.
+//
+
+#import "URThemeConfig.h"
+
+//@implementation URThemeConfig
+//
+//@end
+
